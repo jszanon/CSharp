43cf18c4818f0e1c1ba83c607c5c0b3f0ca64e75
Jessica Sanzoni Zanon <102073253+jszanon@users.noreply.github.com>
2023-01-12 18:21:12 +0000

    Add files via upload

---
+++ JessicaZanon_Class06/JessicaZanon_Class06/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("-*-*-*-*- Zanon Store -*-*-*-*-");
+
+        string clientName;
+        float balance;
+        string product;
+        float price;
+        float remainingBalance;
+
+        clientName = "Giancarlo Pelone";
+        balance = 1250.50f;
+        product = "Apple watch";
+        price = 800.50f;
+        remainingBalance = balance - price;
+
+        Console.WriteLine("Name: " + clientName);
+        Console.WriteLine("Balance: $ " + balance);
+        Console.WriteLine("Product: " + product);
+        Console.WriteLine("Price: $ " + price);
+        Console.WriteLine("Remaining balance: $ " + remainingBalance);
+        Console.ReadLine();
+
+    }
+
+}
+
