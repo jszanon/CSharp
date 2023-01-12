8acd6b87760d8bb69d3b8685b482d11ad512d190
Jessica Sanzoni Zanon <102073253+jszanon@users.noreply.github.com>
2023-01-12 18:18:50 +0000

    Add files via upload

---
+++ JessicaZanon_Class09/JessicaZanon_Class09/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Globalization;
+// if/else - Condiciona a execução de um comando a uma proposição lógica.
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        //int n1 = 2;
+        //int n2 = 3;
+
+        //bool n1MaiorN2 = n1 > n2; //False
+        //bool n1MenorN2 = n1 < n2; //True
+        //bool n1IgualN2 = (n1 == n2);
+
+        //if (n1 > n2)
+        //{
+        //    Console.WriteLine("N1 é maior do que N2.");
+        //}
+        //else if (n1IgualN2)
+        //{
+        //    Console.WriteLine("N1 é igual a N2.");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("N1 é menor do que N2.");
+        //}
+
+
+        //Console.WriteLine(n1MaiorN2);
+        //Console.WriteLine(n1MenorN2);
+        //Console.ReadLine();
+
+        Console.WriteLine("-*-*-*-*-* Zanon Store *-*-*-*-*-");
+        Console.WriteLine();
+        Console.WriteLine("-*-*-*-*-* Customer *-*-*-*-*-");
+        Console.WriteLine("Name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Balance: $ ");
+        double balance = double.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Console.WriteLine("-*-*-*-*-* Product *-*-*-*-*-");
+        Console.WriteLine("Product: ");
+        string product = Console.ReadLine();
+        Console.WriteLine("Price: $ ");
+        double price = double.Parse(Console.ReadLine());
+        Console.WriteLine();
+        double remainingBalance = balance - price;
+        double missingValue = price - balance;
+        bool sucessfulPurchase = remainingBalance > 0; 
+
+        
+        if (sucessfulPurchase)
+        {
+            Console.WriteLine("Sucessful Purchase.");
+            Console.WriteLine("Remaining Balance: $ " + remainingBalance);
+        }
+        else
+        {
+            Console.WriteLine("Purchase not made. Insufficient funds.");
+            Console.WriteLine("The missing value is $ " + missingValue);
+        }
+   
+        Console.ReadLine();
+
+    }
+
+
+}    
+
